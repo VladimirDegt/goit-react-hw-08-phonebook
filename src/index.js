ecf2558e7fdd324bca970a642359368e6de2993e
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-10-11 10:41:05 +0300

    rename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading="null" persistor={persistor}>
-        <BrowserRouter basename="/Phonebook-simple">
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <ChakraProvider>
             <App />
           </ChakraProvider>
